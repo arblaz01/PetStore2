5cd8b07ce17dc2925f1f52626c0254fc9e7628e3
arblaz01 <arblaz01@gmail.com>
2014-03-18 17:08:09 -0400

    Home page added

--- PetStore/PetStore/Models/Pet.cs
+++ PetStore/PetStore/Models/Pet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
@@ -11,6 +12,9 @@ namespace PetStore.Models
         public int ID { get; set; }
         public string Animal { get; set; }
         public string Description { get; set; }
+        [Display(Name = "Received Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime RecDate { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
