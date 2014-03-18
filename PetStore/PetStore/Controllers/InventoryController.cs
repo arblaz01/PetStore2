4ad369fe1ea42b30a3f2f067641cd043849c4154
arblaz01 <arblaz01@gmail.com>
2014-03-18 17:56:25 -0400

    Added Inventory

--- PetStore/PetStore/Controllers/InventoryController.cs
+++ PetStore/PetStore/Controllers/InventoryController.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PetStore.Models;
 
 namespace PetStore.Controllers
 {
     public class InventoryController : Controller
     {
+        private PetDBContext db = new PetDBContext();
         //
         // GET: /Inventory/
         public ActionResult Index()
         {
-            return View();
+            return View(db.Pets.ToList());
         }
 
         public ActionResult Details(string name, int numTimes = 1)
