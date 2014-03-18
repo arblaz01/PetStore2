3f89a51bee9457d216b10d9c8f26967a74760873
arblaz01 <arblaz01@gmail.com>
2014-03-18 16:33:07 -0400

    3rd Attempt to Commit

---
+++ PetStore/PetStore/Models/Pet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PetStore.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public string Animal { get; set; }
+        public string Description { get; set; }
+        public DateTime RecDate { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
